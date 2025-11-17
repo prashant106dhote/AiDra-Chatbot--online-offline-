84e63bf63d7dd321b209e36177e1fceefbbe8af0
prashant dhote <pdhote106@gmail.com>
2025-11-17 15:33:08 +0530

    community chenge

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -9,7 +9,7 @@ const Community = () => {
   const [loading, setLoading] = useState(true)
   const {axios} = useAppContext()
   const fetchImages = async () => {
-    //  yes
+    //  yess
    try {
      const {data} = await axios.get('/api/user/published-images')
      if(data.success){
